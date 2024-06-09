39645814561dbadbb575707bfe374ac36f62fcb7
johanpq <johanp.queiroz@gmail.com>
2024-06-09 14:18:27 -0300

    aprendendo react

---
+++ myAPP1/src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main style={styles}> 
+      <h1>Aprendendo novamente!</h1>
+    </main>
+  );
+}
+ 
