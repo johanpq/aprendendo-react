c821690d9cf6fda5c4d9d214f39b3af79b4ae1d2
johanpq <johanp.queiroz@gmail.com>
2024-06-13 10:34:13 -0300

    aprendendo sobre estilos e css modules

--- react2/src/app/page.jsx
+++ react2/src/app/page.jsx
@@ -1,13 +1,15 @@
 import Pessoa from "@/components/Pessoa";
 import SayMyName from "@/components/SayMyName";
+import StyleGlobal from '@/app/index.css';
 
-export default function Home() {
+export default function Home() { 
 
   const name = "Hanna";
 
   return (
-    <div>
-        <div>Sei la</div> 
+    <div className="green">
+        <div className="red">Sei la</div> 
+
         /*Deixando o props dinâmico
         <SayMyName nome = "Johan"/>
         <SayMyName nome = {name}/>
--- react2/src/components/Pessoa.jsx
+++ react2/src/components/Pessoa.jsx
@@ -1,12 +1,14 @@
+import PersonStyle from "@/components/Pessoa.module.css"
+
 export default function Pessoa({nome, idade, genero, img}) {
     return(
         <div>
             <img src={img} alt={nome} />
 
             <h1>Dados da pessoa:</h1>
-            <p>{nome}</p>
-            <p>{idade}</p>
-            <p>{genero}</p>
+            <p className={PersonStyle.paragraphs}>{nome}</p>
+            <p className={PersonStyle.paragraphs}>{idade}</p>
+            <p className={PersonStyle.paragraphs}>{genero}</p>
         </div>
     )
 }
