39c1cee93e27087aa47b7e26d43bd9d8df83c206
johanpq <johanp.queiroz@gmail.com>
2024-06-26 16:57:09 -0300

    Listando com tsx

--- react4/src/app/page.tsx
+++ react4/src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import ClickBtn from "@/components/ClickBtn";
 import Form from "@/components/Form";
+import Listar from "@/components/Props/Listar";
 
 export default function Home() {
   return (
     <>
         <ClickBtn/>
         <Form/>
+        <Listar/>
     </>
   );
 }
