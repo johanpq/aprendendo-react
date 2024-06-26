20ed264a33482d45a7f6ceae320dee27446bcd13
johanpq <johanp.queiroz@gmail.com>
2024-06-26 15:15:42 -0300

    aprendendo sobre eventos

---
+++ react4/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import ClickBtn from "@/components/ClickBtn";
+import Form from "@/components/Form";
+
+export default function Home() {
+  return (
+    <>
+        <ClickBtn/>
+        <Form/>
+    </>
+  );
+}
